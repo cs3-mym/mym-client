dfe4e0e6cc4de8b8c36aeda8d688861a7a8889c9
Jonathan Chiang <signup.jc13@gmail.com>
2018-03-05 15:05:14 -0600

    updated connections for heroku

--- src/variables/connections.js
+++ src/variables/connections.js
@@ -4,9 +4,9 @@
 
 const DEV_SERVER_URI = 'https://cryptic-shelf-76664.herokuapp.com/';
 
-// const DEV_SITE_ROOT = 'http://localhost:3000/';
+const DEV_SITE_ROOT = 'http://localhost:3000/';
 
-// const PROD_SERVER_URI = 'https://cryptic-shelf-76664.herokuapp.com/';
+const PROD_SERVER_URI = 'https://cryptic-shelf-76664.herokuapp.com/';
 const PROD_SITE_ROOT = 'https://secure-gorge-57528.herokuapp.com/';
 
 module.exports = {
