143053f5ccf9f98db77578d48d2049f0a59fcd7d
Jonathan Chiang <signup.jc13@gmail.com>
2018-03-05 14:53:11 -0600

    updating server connections for heroku

--- src/variables/connections.js
+++ src/variables/connections.js
@@ -1,9 +1,12 @@
 // const DEV_SERVER_URI = 'http://localhost:5000/';
-const DEV_SERVER_URI = 'http://mym-dev.us-west-2.elasticbeanstalk.com/';
-const DEV_SITE_ROOT = 'http://localhost:3000/';
+// const DEV_SERVER_URI = 'http://mym-dev.us-west-2.elasticbeanstalk.com/';
 // const DEV_SITE_ROOT = 'http://localhost:3000/';
 
-const PROD_SERVER_URI = 'https://cryptic-shelf-76664.herokuapp.com/';
+const DEV_SERVER_URI = 'https://cryptic-shelf-76664.herokuapp.com/';
+
+// const DEV_SITE_ROOT = 'http://localhost:3000/';
+
+// const PROD_SERVER_URI = 'https://cryptic-shelf-76664.herokuapp.com/';
 const PROD_SITE_ROOT = 'https://secure-gorge-57528.herokuapp.com/';
 
 module.exports = {
